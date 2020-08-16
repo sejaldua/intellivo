d6796c12568291813644be19e82b31035a95968b
Sejal Dua <sejal.dua@tufts.edu>
2020-08-16 13:17:06 -0700

    config port and heroku yml

--- Intellivo-app/run.py
+++ Intellivo-app/run.py
@@ -1,8 +1,10 @@
 from intellivo_package import app, socketio
+import os
 
 # run the app 
 if __name__ == '__main__':
     # init_db()
     # Change the debug to False when deploying to production
     socketio.run(app, debug=False)
-    app.run(debug=False, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port = port)
