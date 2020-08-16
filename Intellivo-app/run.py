ab85bc2f6ee392a98b327b0f6f3cf5e7d57847dd
Sejal Dua <sejal.dua@tufts.edu>
2020-08-16 13:57:36 -0700

    specify port

--- Intellivo-app/run.py
+++ Intellivo-app/run.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     # init_db()
     # Change the debug to False when deploying to production
     socketio.run(app, debug=False)
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=os.environ.get("PORT", 5000))
